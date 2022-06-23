b2e90cccbceac2f3eddfa71118f5d5a91a1e0dc6
stock-broker <marcello.sanagustin.448@my.csun.edu>
2022-06-22 21:26:00 -0700

    Add project files.

---
+++ spaceships/Data/ShipResult.cs
@@ -0,0 +1,14 @@
+﻿namespace spaceships.Data
+{
+//    Ask
+//I want to be able to search against the swapi.dev starship API to find all starships that contain the
+//search term in the ship name.In the results, I require to know the name, cost, crew and
+//manufacturer.
+    public class ShipResult
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public Ships[] results { get; set; }
+    }
+}
